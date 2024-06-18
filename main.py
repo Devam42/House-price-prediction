bde57c9acff894bd4924bc1388a56cf87317ffa4
Devam42 <dkathane42@gmail.com>
2024-06-18 14:19:09 +0530

    First

--- main.py
+++ main.py
@@ -49,5 +49,5 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)})
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+#if __name__ == "__main__":
+ #   app.run(debug=True, port=5000)
